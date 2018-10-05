55b266266386bba394dd7125e619a02fd43d75e5
Crokxe <nexco@hotmail.com>
2018-10-06 00:30:39 +0300

    v0

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -8,15 +8,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Helpful Constants
-    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
-    private final float LEFT_VOLUME = 1.0f;
-    private final float RIGHT_VOLUME = 1.0f;
-    private final int NO_LOOP = 0;
-    private final int PRIORITY = 0;
-    private final float NORMAL_PLAY_RATE = 1.0f;
-
-    // TODO: Add member variables here
+    private SoundPool mSoundPool;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +23,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+        mSoundPool = new SoundPool(7, AudioManager.STREAM_MUSIC, 0);
 
         // TODO: Load and get the IDs to identify the sounds
-
+        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
 
     }
 
-    // TODO: Add the play methods triggered by the buttons
+    public void playC(View view) {
+        mSoundPool.play(mCSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
 
+    public void playD(View view) {
+        mSoundPool.play(mDSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void playE(View view) {
+        mSoundPool.play(mESoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void playF(View view) {
+        mSoundPool.play(mFSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void playG(View view) {
+        mSoundPool.play(mGSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void playA(View view) {
+        mSoundPool.play(mBSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void playB(View view) {
+        mSoundPool.play(mASoundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    // TODO: Add the play methods triggered by the buttons
 
 
 }
